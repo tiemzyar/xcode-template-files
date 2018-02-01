b157f40132eb0a145ee7cdd137a983f5b1996e79
tmzr84 <tmzr84@users.noreply.github.com>
2018-02-01 10:47:39 +0100

    Created new unit test case templates for Swift and Objective-C

---
+++ TMZR Unit Test Case Class.xctemplate/XCTestCaseObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,39 @@
+//___FILEHEADER___
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <#FrameworkName#>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_testSubclass___
+	#pragma mark Class properties
+
+
+	#pragma mark Instance properties
+
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+#pragma mark Setup and tear-down
+- (void)setUp {
+    [super setUp];
+
+
+}
+
+- (void)tearDown {
+
+
+    [super tearDown];
+}
+
+#pragma mark Test methods
+- (void)testExample {
+
+}
+
+#pragma mark Support methods
+
+
+@end
---
+++ TMZR Unit Test Case Class.xctemplate/XCTestCaseSwift/___FILEBASENAME___.swift
@@ -0,0 +1,36 @@
+//___FILEHEADER___
+
+import UIKit
+import XCTest
+
+@testable import <#FrameworkName#>
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_testSubclass___ {
+	// MARK: Type properties
+
+
+	// MARK: Instance properties
+
+
+	// MARK: Setup and tear-down
+	override func setUp() {
+		super.setUp()
+
+
+	}
+
+	override func tearDown() {
+
+
+		super.tearDown()
+	}
+
+	// MARK: Test methods
+    func testExample() {
+
+    }
+
+	// MARK: Support methods
+
+
+}
