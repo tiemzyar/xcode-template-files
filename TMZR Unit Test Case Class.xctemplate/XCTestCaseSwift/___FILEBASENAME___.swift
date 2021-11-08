14bcbc3d4ab8840577945843ee879a3ba9e4b070
tiemzyar <52572379+tiemzyar@users.noreply.github.com>
2021-11-08 06:54:39 +0100

    Adjusted Swift unit test case template

--- TMZR Unit Test Case Class.xctemplate/XCTestCaseSwift/___FILEBASENAME___.swift
+++ TMZR Unit Test Case Class.xctemplate/XCTestCaseSwift/___FILEBASENAME___.swift
@@ -6,31 +6,37 @@ import XCTest
 @testable import <#FrameworkName#>
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_testSubclass___ {
+	// MARK: Nested types
+
+
 	// MARK: Type properties
 
 
 	// MARK: Instance properties
 
+}
 
-	// MARK: Setup and tear-down
+// MARK: -
+// MARK: Setup and tear-down
+extension ___FILEBASENAMEASIDENTIFIER___ {
 	override func setUp() {
 		super.setUp()
-
-
 	}
 
 	override func tearDown() {
-
-
 		super.tearDown()
 	}
+}
 
-	// MARK: Test methods
-    func testExample() {
+// MARK: -
+// MARK: Supporting methods
+extension ___FILEBASENAMEASIDENTIFIER___ {
+	
+}
 
-    }
-
-	// MARK: Support methods
-
-
+// MARK: -
+// MARK: Tests
+extension ___FILEBASENAMEASIDENTIFIER___ {
+	func test_Example() {
+	}
 }
